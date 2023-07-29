944494afdf75b10d423ba33627cf06fd20789da5
vivek <stephengmre@gmail.com>
2023-07-29 17:47:16 +0530

    fixed3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
     setActiveDiv(id);
   };
   return (
-    <div className='w-screen h-[110vh] bg-slate-100 '>
+    <div className='w-screen h-auto  bg-slate-100 '>
       <div className='flex w-full h-20'>
         <img
           src={Logo}
@@ -16,7 +16,7 @@ const App = () => {
           className='relative w-40 h-auto max-h-[5rem] top-8 left-4 lg:left-20'
         />
       </div>
-      <div className='flex min-h-full flex-col justify-start px-6 lg:px-8 my-8'>
+      <div className='flex min-h-full flex-col justify-start px-6 lg:px-8 mt-10 '>
         <div className='bg-white h-auto mx-auto py-10 px-16 rounded-xl'>
           <div className='sm:mx-auto sm:w-full sm:max-w-sm flex flex-col justify-center items-center'>
             <svg
