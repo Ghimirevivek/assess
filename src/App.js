4ac765249f4dba7ccd49c8a38e2126dbfb250a12
vivek <stephengmre@gmail.com>
2023-07-29 17:52:34 +0530

    fixed5

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ const App = () => {
         <img
           src={Logo}
           alt='Wobot'
-          className='relative w-40 h-auto max-h-[5rem] top-8 left-4 lg:left-20'
+          className='relative w-40 h-auto max-h-[5rem] top-2 left-4 lg:left-20'
         />
       </div>
-      <div className='flex min-h-full flex-col justify-start px-6 lg:px-8 mt-6 lg:mt-0 '>
+      <div className='flex min-h-full flex-col justify-start px-6 lg:px-8 mt-4 lg:mt-0 '>
         <div className='bg-white h-auto mx-auto py-10 px-16 rounded-xl'>
           <div className='sm:mx-auto sm:w-full sm:max-w-sm flex flex-col justify-center items-center'>
             <svg
